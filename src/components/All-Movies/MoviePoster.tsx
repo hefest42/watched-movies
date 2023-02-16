09d3f2899ab1bfb9ffa3ee8b7171b1beda91f7dd
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-16 19:57:31 +0100

    changing MOVIES to work with the API

--- src/components/All-Movies/MoviePoster.tsx
+++ src/components/All-Movies/MoviePoster.tsx
@@ -7,30 +7,14 @@ interface MoviePosterProps {
     movie: {
         id: string;
         name: string;
-        releaseYear: string;
-        runtime: string;
-        summary: string;
-        director: string[];
-        writers: string[];
-        actors: string[];
-        genre: string[];
-        links: {
-            imdb: string;
-            rottenTomatoes: string;
-            poster: string;
-        };
+        poster: string;
     };
 }
 
 const MoviePoster = ({ movie }: MoviePosterProps) => {
     return (
         <Link to="movie/1" className="w-full h-full p-2 border-[1px] border-black bg-yellow-400">
-            <img
-                loading="lazy"
-                src={movie.links.poster}
-                alt="movie poster"
-                className="movie w-full h-auto object-cover"
-            />
+            <img loading="lazy" src={movie.poster} alt="movie poster" className="movie w-full h-auto object-cover" />
         </Link>
     );
 };
