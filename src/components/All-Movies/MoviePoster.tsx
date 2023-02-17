63c9c4f8bba64dcbe6ec3804da43aedfdb02421a
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-17 20:03:57 +0100

    Update MoviePoster.tsx

--- src/components/All-Movies/MoviePoster.tsx
+++ src/components/All-Movies/MoviePoster.tsx
@@ -14,7 +14,7 @@ interface MoviePosterProps {
 
 const MoviePoster = ({ movie }: MoviePosterProps) => {
     return (
-        <Link to="movie/1" className="w-full h-full p-2 border-[1px] border-black bg-yellow-400">
+        <Link to="movie/1" className={`w-full h-full p-2 border-[1px] border-black bg-yellow-400`}>
             <img loading="lazy" src={movie.poster} alt="movie poster" className="movie w-full h-auto object-cover" />
         </Link>
     );
