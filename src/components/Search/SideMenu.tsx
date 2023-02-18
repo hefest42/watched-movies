f8aa3d95ace97e34bf32f28968fe450cf95cdd00
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-18 16:58:38 +0100

    added a toggle for light/dark theme

--- src/components/Search/SideMenu.tsx
+++ src/components/Search/SideMenu.tsx
@@ -64,7 +64,14 @@ const SideMenu = ({ showSideMenu, setShowSideMenu, addFilters }: SideMenuProps) 
 
             <div>
                 <p>Dark/White</p>
-                <div></div>
+                <div>
+                    <input type="checkbox" className="checkbox" id="checkbox" />
+                    <label htmlFor="checkbox" className="checkbox-label">
+                        <i className="fas fa-moon"></i>
+                        <i className="fas fa-sun"></i>
+                        <span className="ball"></span>
+                    </label>
+                </div>
             </div>
         </div>
     );
