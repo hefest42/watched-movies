eed640c41a683955ec1fe9d21b6bfe439e78a87b
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-21 17:46:08 +0100

    adding external links

--- src/components/Movie-Page/Ratings.tsx
+++ src/components/Movie-Page/Ratings.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { Link, useLocation } from "react-router-dom";
+
 import { FaImdb } from "react-icons/fa";
 import { SiRottentomatoes } from "react-icons/si";
 import { GiPopcorn } from "react-icons/gi";
@@ -18,7 +20,44 @@ interface RatingsProps {
 }
 
 const Ratings = ({ ratings }: RatingsProps) => {
+    const location = useLocation();
     const [movieRatings, setMovieRatings] = useState<MovieRating[]>([]);
+
+    const ratingsHandler = (rating: MovieRating) => {
+        const { id } = location.state;
+
+        switch (rating.source) {
+            case "imdb":
+                return (
+                    <div>
+                        <a href={`https://www.imdb.com/title/${id}`} target="_blank" rel="noopener noreferrer">
+                            <FaImdb />
+                        </a>
+                        <p>{`${rating.value}/10`}</p>
+                    </div>
+                );
+
+            case "tomatoes":
+                return (
+                    <div>
+                        <a href="www.imdb/" target="_blank" rel="noopener noreferrer">
+                            <SiRottentomatoes />
+                        </a>
+                        <p>{`${rating.value}/100`}</p>
+                    </div>
+                );
+
+            case "tomatoesaudience":
+                return (
+                    <div>
+                        <a href="www.imdb/" target="_blank" rel="noopener noreferrer">
+                            <GiPopcorn />
+                        </a>
+                        <p>{`${rating.value}/100`}</p>
+                    </div>
+                );
+        }
+    };
 
     useEffect(() => {
         const filteredRatings = ratings.filter((rating) => {
@@ -29,7 +68,13 @@ const Ratings = ({ ratings }: RatingsProps) => {
         setMovieRatings(filteredRatings);
     }, ratings);
 
-    return <div></div>;
+    return (
+        <div>
+            {movieRatings.map((rating, i) => (
+                <div key={i}>{ratingsHandler(rating)}</div>
+            ))}
+        </div>
+    );
 };
 
 export default Ratings;
