e15b709b5c2be5e3190cc7266ebd245740a6bbb6
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-21 14:31:22 +0100

    changed how movie info is displayed

--- src/components/Movie-Page/PosterTitleInfo.tsx
+++ src/components/Movie-Page/PosterTitleInfo.tsx
@@ -1,23 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface MovieProps {
     poster: string;
     title: string;
-    info: (string | number)[];
+    info: any[];
 }
 
 const PosterTitleInfo = ({ poster, title, info }: MovieProps) => {
-    useEffect(() => {}, []);
+    const [movieInformatiom, setMovieInformation] = useState<string[]>([]);
+
+    useEffect(() => {
+        const [type, releaseDate, runtime, certification, ageRating] = info;
+        const date = new Date(releaseDate);
+        const formatedDate = new Intl.DateTimeFormat("en-UK").format(date);
+        const movieType = `${type[0].toUpperCase()}${type.slice(1)}`;
+        const esrb = `${certification}-${ageRating}`;
+        setMovieInformation([movieType, formatedDate, runtime, esrb]);
+    }, [info]);
 
     return (
         <div className="">
             <img src={poster} alt={`${title} poster`} />
+            <div>{movieInformatiom.join(" - ")}</div>
             <h1>{title}</h1>
-            <div>
-                {info.map((info) => (
-                    <div>{info}</div>
-                ))}
-            </div>
         </div>
     );
 };
