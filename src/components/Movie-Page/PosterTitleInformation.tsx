b1c894a024e853901755c265acaa4984ce3c17c3
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-21 17:19:28 +0100

    poster & title added

--- src/components/Movie-Page/PosterTitleInformation.tsx
+++ src/components/Movie-Page/PosterTitleInformation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface PosterTitleInformationProps {
     poster: string;
@@ -12,11 +12,25 @@ interface PosterTitleInformationProps {
 }
 
 const PosterTitleInformation = ({ poster, title, info }: PosterTitleInformationProps) => {
+    const [movieInformation, setMovieInformation] = useState<string[]>([]);
+
+    useEffect(() => {
+        const { type, runtime, release, rating } = info;
+
+        const movieType = `${type[0].toUpperCase()}${type.slice(1)}`;
+        const movieRuntime = `${runtime}m`;
+
+        const releaseDate = new Date(release);
+        const formattedDate = new Intl.DateTimeFormat("eu-UK").format(releaseDate);
+
+        setMovieInformation([movieType, formattedDate, movieRuntime, rating]);
+    }, [info]);
+
     return (
         <>
             <img src={poster} alt={`movie poster`} />
-            <div></div>
-            <h1>{title}</h1>
+            <div className="px-2 text-sm">{movieInformation.join(" - ")}</div>
+            <h1 className="px-2 text-3xl">{title}</h1>
         </>
     );
 };
