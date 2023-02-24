884376dd2d89f19776df6abf8488e3682e801b3a
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-24 13:16:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import HomePage from "./components/Pages/HomePage";
 import MoviePage from "./components/Pages/MoviePage";
 import WrongURL from "./components/Pages/WrongURL";
 
+// TODO fix issue with multiple directors
+// TODO fix issue with rottentomatoes link
+// TODO change the arrow size on actors arrow down svg
+// TODO potentially change the poster size of md:
+// TODO see if you can add lazy loading for the whole movie page
+// TODO change font sizes and font
+// TODO add search function
+// TODO add a home page button to movie page
+
 function App() {
     return (
         <div className="relative w-full bg-gray-900 text-yellow-300">
