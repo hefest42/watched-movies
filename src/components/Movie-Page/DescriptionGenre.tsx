bcb90cc2126947df44267a7e4d7a8a08db435397
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-25 15:33:05 +0100

    size of genres changed

--- src/components/Movie-Page/DescriptionGenre.tsx
+++ src/components/Movie-Page/DescriptionGenre.tsx
@@ -12,7 +12,7 @@ const DescriptionGenre = ({ description, genres }: DescriptionGenreProps) => {
                 {genres.map((genre) => (
                     <div
                         key={genre}
-                        className="px-4 py-1 mr-2 rounded-3xl border border-yellow-500 sm:px-6 sm:py-2 sm:text-lg"
+                        className="px-2 py-1 mr-2 rounded-3xl border border-yellow-500 sm:px-4 sm:py-1 sm:text-lg"
                     >
                         {genre}
                     </div>
