b54802940aa18d54d19bcbbbb9185913d6d8cb6c
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-27 17:14:21 +0100

    added theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -7,16 +7,38 @@ import MoviePage from "./components/Pages/MoviePage";
 import WrongURL from "./components/Pages/WrongURL";
 
 //TODO change colors for dark/light theme
-//TODO add error handling for movie page
+
+interface ThemeContextValues {
+    theme: boolean;
+    setTheme: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const ThemeContext = createContext<ThemeContextValues>({
+    theme: false,
+    setTheme: () => {},
+});
 
 function App() {
+    const [theme, setTheme] = useState(false);
+
     return (
-        <div className="relative w-full h-min-screen pb-8 bg-gray-900 text-yellow-300">
-            <Routes>
-                <Route path="" element={<HomePage />} />
-                <Route path="movie/:id" element={<MoviePage />} />
-                <Route path="*" element={<WrongURL />} />
-            </Routes>
+        <div
+            className={`relative w-full h-min-screen pb-8 text-gray-800 ${
+                !theme ? "text-gray-800" : "bg-slate-800 text-slate-100"
+            }`}
+        >
+            <ThemeContext.Provider
+                value={{
+                    theme: theme,
+                    setTheme: setTheme,
+                }}
+            >
+                <Routes>
+                    <Route path="" element={<HomePage />} />
+                    <Route path="movie/:id" element={<MoviePage />} />
+                    <Route path="*" element={<WrongURL />} />
+                </Routes>
+            </ThemeContext.Provider>
         </div>
     );
 }
--- src/components/Search/ThemeToggle.tsx
+++ src/components/Search/ThemeToggle.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { ThemeContext } from "../../App";
 
 const ThemeToggle = () => {
+    const { theme, setTheme } = useContext(ThemeContext);
+
     return (
         <div className="flex justify-between p-2">
-            <p>Dark / Light</p>
+            <p>Light / Dark</p>
             <div>
-                <input type="checkbox" className="checkbox" id="checkbox" />
+                <input
+                    type="checkbox"
+                    checked={theme}
+                    className="checkbox"
+                    id="checkbox"
+                    onClick={() => setTheme((state) => !state)}
+                />
                 <label htmlFor="checkbox" className="checkbox-label">
                     <i className="fas fa-moon"></i>
                     <i className="fas fa-sun"></i>
