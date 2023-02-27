a7b42a55e66b2c844edcfbe780c86ea770a8f2df
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-27 16:21:24 +0100

    added bottom padding

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import WrongURL from "./components/Pages/WrongURL";
 
 function App() {
     return (
-        <div className="relative w-full bg-gray-900 text-yellow-300">
+        <div className="relative w-full h-min-screen pb-8 bg-gray-900 text-yellow-300">
             <Routes>
                 <Route path="" element={<HomePage />} />
                 <Route path="movie/:id" element={<MoviePage />} />
